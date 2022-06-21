c9e3c8a1ff0278a3da2c7b41309a00b3292887ab
Moringa School <karanjasam210@gmail.com>
2022-06-21 12:18:27 +0300

    add urlpaths

--- watch/urls.py
+++ watch/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns=[
+   
    path('',views.index, name='index'),
    path('logout/',auth_views.LogoutView.as_view(template_name = 'registration/logout.html')),
    path('update-profile',views.update_profile, name='update_profile'), 
